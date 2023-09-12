f31abb88bd3a2133b01f74c5d4fa725669e0d3cf
Sami Hakani <sami.hakani@gmail.com>
2023-09-12 19:34:30 -0400

    Created nav bar buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 content = document.querySelector('#content');
 
-// header = document.createElement('div.header');
+// Background image
 backgroundImage = document.createElement('img');
 backgroundImage.classList.add('background-image')
 backgroundImage.src = '../src/deli.jpg'
 
 content.appendChild(backgroundImage);
+
+// Header
+header = document.createElement('div');
+header.classList.add('header');
+
+headerText = document.createElement('h1');
+headerText.textContent = 'Fuhgettaboutit';
+
+navBar = document.createElement('nav');
+navLinks = ['Home', 'Menu', 'Contact'];
+for(let i = 0; i < navLinks.length; i++) {
+    btn = document.createElement('button');
+    btn.classList.add(`page-${i}`);
+    navBar.appendChild(btn);
+}
+
+header.appendChild(headerText);
+header.appendChild(navBar);
+content.appendChild(header);
