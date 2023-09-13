e451790956951980557a4702872260f85d1b521a
Sami Hakani <sami.hakani@gmail.com>
2023-09-13 13:46:16 -0400

    Styled contact page

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,29 @@
+// import
+import addy from './assets/address.png';
+
 function contact() {
-    console.log('contact');
+    let canvas = document.querySelector('#canvas');
+    canvas.classList.remove('homepage');
+    canvas.classList.remove('menu');
+    canvas.classList.add('contact');
+
+    // console.log(canvas.firstChild);
+    while(canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+
+    let phone = document.createElement('p');
+    let address = document.createElement('p');
+    phone.textContent = '📞 123 456 7890';
+    address.textContent = '🏠 2880 Broadway, New York, NY 10025';
+
+    const mapPic = new Image();
+    mapPic.src = addy;
+    mapPic.classList.add('address');
+
+    canvas.appendChild(phone);
+    canvas.appendChild(address);
+    canvas.appendChild(mapPic);
 }
 
 export {contact};
