19b6eecb9942b5cc47107a7596e973efbcf6e490
Sami Hakani <sami.hakani@gmail.com>
2023-09-13 13:03:19 -0400

    Added menu to dictionary and DOM looped over

--- src/menu.js
+++ src/menu.js
@@ -12,11 +12,37 @@ function menu() {
         canvas.removeChild(canvas.firstChild);
     }
 
-    let menuItems = ['Reuben', 'Classic', 'Hot & Red', 'Beastwich', 'Wetsider', 'Godmother', 'Italiano', 'Melty', 'Noo Yoiker', 'Olympia', 'American', 'Big Stack', 'Chicken Club', 'Supawich'];
-    for(let i = 0; i < menuItems.length; i++) {
+    let menuContents = {
+        'Reuben': 'Traditional, turkey, or pastrami served grilled with kraut & swiss',
+        'Classic': 'Roast beef, corned beef, turkey, slaw, 1000 island',
+        'Hot & Red': 'Cajun roast beef, bbq chicken, pepper jack cheese, red pepper hummus',
+        'Beastwich': 'Turkey, pepperoni, pepper jack cheese',
+        'Westsider': 'Hot corned beef, hot pastrami, slaw, 1000 island',
+        'Godmother': 'Salami, gabagool, prosecution, provolone cheese',
+        'Italiano': 'Hickory turkey, salami, capicola, cheese',
+        'Melty': 'Hot tuna or chicken salad, cheese',
+        'Noo Yoiker': 'Hot corned beef, hot pastrami, swiss cheese',
+        'Olympia': 'Ham, salami, bologna, capicola, turkey, pepperoni, provolone cheese',
+        'American': 'Italiano topped off with hot water',
+        'Big Stack': 'Jerk & blackened turkey, pork sausage, bacon, provolone, pepper jack, spicy mayo',
+        'Chicken Club': 'Triple decker chicken, ham, bacon, & cheese',
+        'Supawich': 'Turkey, feta cheese, greek mayo',
+    };
+
+    for(let i in menuContents) {
         let card = document.createElement('div');
         card.classList.add('card');
-        card.textContent = `card ${i}`;
+        // card.textContent = `card ${i}`;
+        let menuTitle = document.createElement('div');
+        menuTitle.classList.add('card-title');
+        menuTitle.textContent = i;
+
+        let menuContent = document.createElement('div');
+        menuContent.classList.add('card-content');
+        menuContent.textContent = menuContents[i];
+
+        card.appendChild(menuTitle);
+        card.appendChild(menuContent);
 
         canvas.appendChild(card);
     }
